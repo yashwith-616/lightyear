7243329f263b53c03703f04e1ff79d91bede9643
Yashwith D Alva <yashwithalva10@gmail.com>
2025-11-28 02:04:27 +0530

    Clang fixed; msvc directory fix required

--- cmake/toolchains/windows-clang.cmake
+++ cmake/toolchains/windows-clang.cmake
@@ -1,9 +1,7 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(CMAKE_C_COMPILER clang-cl)
-set(CMAKE_CXX_COMPILER clang-cl)
 set(CMAKE_CXX_STANDARD 20)
 
-# Match MSVC ABI
-set(VCPKG_TARGET_TRIPLET "x64-windows-static-md" CACHE STRING "")
+# Match GNU ABI
+set(VCPKG_TARGET_TRIPLET "x64-windows" CACHE STRING "")
